1f391d7f0ae311488631a4c480fc6db5c6713c39
tatkei1 <maggietatkei@gmail.com>
2024-05-19 11:53:47 -0500

    Learn Git2

--- src/test/java/variousConcepts/LearnGit2.java
+++ src/test/java/variousConcepts/LearnGit2.java
@@ -3,16 +3,18 @@ package variousConcepts;
 public class LearnGit2 {
 
 	public static void main(String[] args) {
-		
 
 	}
-public void printMe1() {
-	
-}
-public void printMe2() {
-	
-}
-public void printMe3() {
-	
-}
+
+	public void printMe1() {
+		System.out.println("am the first print!");
+	}
+
+	public void printMe2() {
+		System.out.println("am print 2");
+	}
+
+	public void printMe3() {
+
+	}
 }
