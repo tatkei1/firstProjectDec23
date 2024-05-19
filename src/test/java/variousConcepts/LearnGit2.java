e120fc28d5c32f56e2536729f081d02735f6b9e6
tatkei1 <maggietatkei@gmail.com>
2024-05-19 11:31:16 -0500

    Learn Git

---
+++ src/test/java/variousConcepts/LearnGit2.java
@@ -0,0 +1,18 @@
+package variousConcepts;
+
+public class LearnGit2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+public void printMe1() {
+	
+}
+public void printMe2() {
+	
+}
+public void printMe3() {
+	
+}
+}
